e4b8a157ec816b2fc4f7df00599e6cacd2c3a339
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-20 17:05:34 +0530

    Adding 2 Numbers

--- test1/calc/views.py
+++ test1/calc/views.py
@@ -5,3 +5,10 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html', {'name': 'Vishal'})
+
+def add(request):
+    Val1 = request.GET['num1']
+    Val2 = request.GET['num2']
+    Res = int(Val1) + int(Val2)
+
+    return render(request, 'result.html', {'result': Res})
