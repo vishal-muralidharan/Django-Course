d568ef2e86433a794a3c82cf32496456a252b122
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-21 10:22:10 +0530

    Settings to Store Image Files

--- test1/test1/settings.py
+++ test1/test1/settings.py
@@ -131,3 +131,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
