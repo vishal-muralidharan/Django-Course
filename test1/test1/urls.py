bfaae4d97101c8daa6d64c521cdc80f54e6c11df
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-21 10:41:43 +0530

    Registration Page

--- test1/test1/urls.py
+++ test1/test1/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('travello.urls')),
     path('calc/', include('calc.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls), 
+    path('accounts/', include('accounts.urls'))
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
